9702f7c65af648a14eaa137e6adc02fd05df8acd
Dulio Denis <dulio.denis@yahoo.com>
2018-06-30 13:58:54 -0400

    Added an AppBar widget

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -23,6 +23,10 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+
+      ),
+
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
